9924675ac9eaf77018dca49b0b22ce2cdd643602
wangjiyu <13429183453@163.com>
2019-04-21 16:57:56 +0800

    Git_Test01

---
+++ git_test01/src/GitTest01.java
@@ -0,0 +1,9 @@
+/**
+ * @author StudyWang
+ * @date 2019/4/21 - 16:56
+ */
+public class GitTest01 {
+    public static void main(String[] args) {
+        System.out.println("\"GitTest01\" = " + "GitTest01");
+    }
+}
