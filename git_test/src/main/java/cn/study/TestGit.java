4f17ae442452b500374ebcc29444bcf55b3c456d
wangjiyu <13429183453@163.com>
2019-04-21 15:26:45 +0800

    test commit git

---
+++ git_test/src/main/java/cn/study/TestGit.java
@@ -0,0 +1,11 @@
+package cn.study;
+
+/**
+ * @author StudyWang
+ * @date 2019/4/21 - 15:10
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("null = " + null);
+    }
+}
