87dc447c87ce3da1fb29f81acbf9dad4394c2330
wangjiyu <13429183453@163.com>
2019-04-21 16:00:53 +0800

    test_clone_modify_push

--- git_test/src/main/java/cn/study/TestGit.java
+++ git_test/src/main/java/cn/study/TestGit.java
@@ -8,5 +8,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("null = " + null);
         System.out.println("\"modify\" = " + "modify");
+        System.out.println("\"modify\" = " + "modify");
     }
 }
